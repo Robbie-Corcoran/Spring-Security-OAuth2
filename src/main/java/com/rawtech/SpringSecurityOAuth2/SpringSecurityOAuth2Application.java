757e9c2ba9e064703e647f1323773d2dab99ec6b
Robbie Corcoran <codewithrobbie@gmail.com>
2023-11-13 13:26:24 +0100

    Project Initialized.

---
+++ src/main/java/com/rawtech/SpringSecurityOAuth2/SpringSecurityOAuth2Application.java
@@ -0,0 +1,13 @@
+package com.rawtech.SpringSecurityOAuth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityOAuth2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityOAuth2Application.class, args);
+	}
+
+}
