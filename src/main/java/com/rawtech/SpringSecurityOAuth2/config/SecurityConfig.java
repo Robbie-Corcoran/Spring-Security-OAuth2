d1b15577db956f053dfe7b5fedfe450cdf07bf7c
Robbie Corcoran <codewithrobbie@gmail.com>
2023-11-13 13:37:22 +0100

    Added auth for any page other than the "/" mapping

--- src/main/java/com/rawtech/SpringSecurityOAuth2/config/SecurityConfig.java
+++ src/main/java/com/rawtech/SpringSecurityOAuth2/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.rawtech.SpringSecurityOAuth2.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -12,7 +13,13 @@ public class SecurityConfig {
 
     @Bean
     SecurityFilterChain securityFilterChain (HttpSecurity httpSecurity) throws Exception {
-        return httpSecurity.
-                build();
+        return httpSecurity
+                .authorizeHttpRequests(auth -> {
+                    auth.requestMatchers("/").permitAll();
+                    auth.anyRequest().authenticated();
+                })
+                .oauth2Login(Customizer.withDefaults())
+                .formLogin(Customizer.withDefaults())
+                .build();
     }
 }
