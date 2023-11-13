662ec1e9167870c29b090d9fd007100ff561e4a7
Robbie Corcoran <codewithrobbie@gmail.com>
2023-11-13 13:29:13 +0100

    Added controller with home and secured mapping.

---
+++ src/main/java/com/rawtech/SpringSecurityOAuth2/controller/HomeController.java
@@ -0,0 +1,18 @@
+package com.rawtech.SpringSecurityOAuth2.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Hello, Home!";
+    }
+
+    @GetMapping("/secured")
+    public String secured() {
+        return "Hello, Secured!";
+    }
+}
